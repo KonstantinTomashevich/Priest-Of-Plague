d73f314f3748d3ba929cbcf9ac8eb3c58fa10fa7
Konstantin Tomashevich <ekit.mail@gmail.com>
2018-04-03 19:30:12 +0300

    Item: implement.

---
+++ Assets/Source/Items/Item.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+namespace PriestOfPlague.Source.Items
+{
+    public class Item
+    {
+        public Item (int itemTypeId, float charge = 0, int level = 0)
+        {
+            _itemTypeId = itemTypeId;
+            Charge = charge;
+            Level = level;
+        }
+
+        public int ItemTypeId => _itemTypeId;
+        public float Charge
+        {
+            get { return _charge; }
+            set
+            {
+                Debug.Assert (value >= 0.0f); 
+                _charge = value; 
+            }
+        }
+
+        public int Level
+        {
+            get { return _level; }
+            set
+            {
+                Debug.Assert (value >= 0);
+                _level = value;
+            }
+        }
+
+        private int _itemTypeId;
+        private float _charge;
+        private int _level;
+    }
+}
+
+
