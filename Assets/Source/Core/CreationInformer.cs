674395a076d0b029bda4ede8ef9d44c6b3600085
Konstantin Tomashevich <ekit.mail@gmail.com>
2018-04-21 18:21:59 +0300

    Add CreationInformer and make it base class for Unit.

---
+++ Assets/Source/Core/CreationInformer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using PriestOfPlague.Source.Hubs;
+
+namespace PriestOfPlague.Source.Core
+{
+    public class CreationInformer : MonoBehaviour
+    {
+        public const string EventObjectCreated = "ObjectCreated";
+        
+        protected void Start ()
+        {
+            EventsHub.Instance.SendGlobalEvent (EventObjectCreated, this);
+        }
+    }
+}
