2b4152f7d36547b698a627c7625dc1cc9dca367e
Konstantin Tomashevich <ekit.mail@gmail.com>
2018-05-05 11:26:46 +0300

    Refactor lineage and modifiers types containers.

--- Assets/Source/Unit/CharacterModifiersContainer.cs
+++ Assets/Source/Unit/CharacterModifiersContainer.cs
@@ -1,92 +1,72 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 
 
 namespace PriestOfPlague.Source.Unit
 {
-    public enum CharactiristicsEnum { Vitality = 0, Luck, Agility, Strength, Intelligence }
-    public enum BuffsAndDebuffsEnum { Healing = 0, Satiety, Magic_support, Weakening, Poisoning, Disease, Paralysis }
-
     public class CharacterModifiersContainer : MonoBehaviour
     {
-        public CharacterModifiersContainer()
+        private List <CharacterModifier> _modifiersArray;
+
+        public CharacterModifier GetBuff (int id)
         {
-            SetBuffs();
+            if (id >= 0 && id < _modifiersArray.Count)
+            {
+                return _modifiersArray [id];
+            }
+
+            throw new IndexOutOfRangeException ("Unknown modifier id!");
         }
 
-        const int numberOfBuffsAndDebuffs = 7;
-        CharacterModifier[] dict = new CharacterModifier[numberOfBuffsAndDebuffs];
-        /// <summary>
-        /// Даёт баффы по индексу
-        /// </summary>
-        /// <param name="IndexIn">Уникальный индекс баффа</param>
-        /// <returns>бафф</returns>
-        public CharacterModifier GetBuff(int IndexIn) //Меня терзают смутные сомнения о правильности такой реализации...
+        private void Start ()
         {
-            if (dict[IndexIn] != null)
-                return dict[IndexIn];
-            else throw new System.Exception("Неверный индекс!");
+            _modifiersArray = new List <CharacterModifier> ();
         }
-        /// <summary>
-        /// Устанавливает все баффы
-        /// </summary>
+
+        /* Move to XML
         public void SetBuffs()
         {
             //Лечение
             CharacterModifier a = new CharacterModifier("Лечение", 5);
-            a.SetArr(0, 0, 0, 0, 0);
+            a.SetCharacteristicsChanges(0, 0, 0, 0, 0);
             a.PlusRegen = 3;
-            a.BuffsForCancel.Add((int)BuffsAndDebuffsEnum.Weakening);
-            a.BuffsForCancel.Add((int)BuffsAndDebuffsEnum.Disease);
-            dict[(int)BuffsAndDebuffsEnum.Healing] = a;
+            a.BuffsToCancel.Add((int)BuffsAndDebuffsEnum.Weakening);
+            a.BuffsToCancel.Add((int)BuffsAndDebuffsEnum.Disease);
+            modifiersArray[(int)BuffsAndDebuffsEnum.Healing] = a;
 
             //Сытость
             a = new CharacterModifier("Сытость", 5);
-            a.SetArr(1, 0, 1, 1, 0);
-            dict[(int)BuffsAndDebuffsEnum.Satiety] = a;
+            a.SetCharacteristicsChanges(1, 0, 1, 1, 0);
+            modifiersArray[(int)BuffsAndDebuffsEnum.Satiety] = a;
 
             //Магическая поддержка
             a = new CharacterModifier("Магическая поддержка", 5);
-            a.SetArr(1, 1, 1, 1, 1);
-            dict[(int)BuffsAndDebuffsEnum.Magic_support] = a;
+            a.SetCharacteristicsChanges(1, 1, 1, 1, 1);
+            modifiersArray[(int)BuffsAndDebuffsEnum.Magic_support] = a;
 
             //Ослаблен
             a = new CharacterModifier("Ослабление", 5);
-            a.SetArr(-1, 0, -1, -1, 0);
-            dict[(int)BuffsAndDebuffsEnum.Weakening] = a;
+            a.SetCharacteristicsChanges(-1, 0, -1, -1, 0);
+            modifiersArray[(int)BuffsAndDebuffsEnum.Weakening] = a;
 
             //Отравлен
             a = new CharacterModifier("Отравление", 5);
-            a.SetArr(-1, 0, -1, -1, 0);
+            a.SetCharacteristicsChanges(-1, 0, -1, -1, 0);
             a.PlusRegen = -1;
-            dict[(int)BuffsAndDebuffsEnum.Poisoning] = a;
+            modifiersArray[(int)BuffsAndDebuffsEnum.Poisoning] = a;
 
             //Болен 
             a = new CharacterModifier("Болезнь", 5);
-            a.SetArr(-1, -1, -1, -1, -1);
-            dict[(int)BuffsAndDebuffsEnum.Disease] = a;
+            a.SetCharacteristicsChanges(-1, -1, -1, -1, -1);
+            modifiersArray[(int)BuffsAndDebuffsEnum.Disease] = a;
 
             //Парализован(Опять же не реализовать, так как нужно понимание действий (класс unit))
             a = new CharacterModifier("Паралич", 5);
-            a.SetArr(0, 0, 0, 0, 0);
-            dict[(int)BuffsAndDebuffsEnum.Paralysis] = a;
-        }
-
-        /// <summary>
-        /// Класс содержит информацию о баффах/дебаффах
-        /// </summary>
-
-        // Use this for initialization
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
+            a.SetCharacteristicsChanges(0, 0, 0, 0, 0);
+            modifiersArray[(int)BuffsAndDebuffsEnum.Paralysis] = a;
+        }**/
     }
 }
