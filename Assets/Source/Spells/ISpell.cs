b1c4b1f9a817e989cea65018e1c772bc0efa3e90
Konstantin Tomashevich <ekit.mail@gmail.com>
2018-05-10 11:28:51 +0300

    Add ISpell.

---
+++ Assets/Source/Spells/ISpell.cs
@@ -0,0 +1,15 @@
+﻿using PriestOfPlague.Source.Hubs;
+using PriestOfPlague.Source.Unit;
+using UnityEngine;
+
+namespace PriestOfPlague.Source.Spells
+{
+    public interface ISpell
+    {
+        int Id { get; }
+        bool CanCast (Unit.Unit unit);
+        void Cast (Unit.Unit caster, UnitsHub unitsHub, object parameter);
+        Sprite Icon { get; set; }
+        string Info { get; }
+    }
+}
