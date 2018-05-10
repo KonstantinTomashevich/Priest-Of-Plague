37cbb8e9bbcbbcb307757b9e27b0614f0b5ffa0f
Konstantin Tomashevich <ekit.mail@gmail.com>
2018-05-10 11:28:59 +0300

    Add SpellsContainer.

---
+++ Assets/Source/Spells/SpellsContainer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace PriestOfPlague.Source.Spells
+{
+    public class SpellsContainer : MonoBehaviour
+    {
+        public SpellsContainer ()
+        {
+            _spells = new Dictionary <int, ISpell> ();
+        }
+
+        public bool AddSpell (ISpell spell)
+        {
+            if (Spells.ContainsKey (spell.Id))
+            {
+                return false;
+            }
+
+            Spells [spell.Id] = spell;
+            return true;
+        }
+
+        public bool RemoveSpell (ISpell spell)
+        {
+            return Spells.Remove (spell.Id);
+        }
+
+        public Dictionary <int, ISpell> Spells => _spells;
+        private Dictionary <int, ISpell> _spells;
+    }
+}
