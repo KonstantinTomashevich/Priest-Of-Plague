1e5a495b20fc20da99bcc2537d86742c217c24ed
Konstantin Tomashevich <ekit.mail@gmail.com>
2018-05-19 18:54:17 +0300

    UnitsHubCriteria.MaxDistanceAndMaxAngle: fix angle calculation.

---
+++ Assets/Source/Core/MathUtils.cs
@@ -0,0 +1,10 @@
+﻿namespace PriestOfPlague.Source.Core
+{
+    public static class MathUtils
+    {
+        public static float AngleFrom_0_360To_m180_180 (float angle)
+        {
+            return angle > 180.0f ? angle - 360.0f : angle;
+        }
+    }
+}
