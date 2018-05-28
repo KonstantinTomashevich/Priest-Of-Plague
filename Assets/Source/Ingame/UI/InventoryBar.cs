1321ca6c3b91f9962ab5127cbf723787691cff5b
Konstantin Tomashevich <ekit.mail@gmail.com>
2018-05-28 23:41:39 +0300

    Add missing namespace to ui scripts.

--- Assets/Source/Ingame/UI/InventoryBar.cs
+++ Assets/Source/Ingame/UI/InventoryBar.cs
@@ -9,143 +9,148 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class InventoryBar : MonoBehaviour
+namespace PriestOfPlague.Source.Ingame.UI
 {
-    public Unit SourceUnit;
-    public GameObject ItemUIItemPrefab;
-    public GameObject ItemsListContentObject;
-    public GameObject SelectedItemInfoObject;
-    public GameObject DestroySelectedObjectButtonObject;
-
-    public int SelectedItemId { get; private set; }
-    private Dictionary <int, GameObject> _icons;
-
-    public void Toggle ()
+    public class InventoryBar : MonoBehaviour
     {
-        gameObject.SetActive (!gameObject.activeInHierarchy);
-    }
+        public Unit.Unit SourceUnit;
+        public GameObject ItemUIItemPrefab;
+        public GameObject ItemsListContentObject;
+        public GameObject SelectedItemInfoObject;
+        public GameObject DestroySelectedObjectButtonObject;
 
-    public void DestroySelectedItem ()
-    {
-        if (SelectedItemId != -1 && SourceUnit.MyStorage [SelectedItemId] != null)
+        public int SelectedItemId { get; private set; }
+        private Dictionary <int, GameObject> _icons;
+
+        public void Toggle ()
         {
-            SourceUnit.MyStorage.RemoveItem (SourceUnit.MyStorage [SelectedItemId]);
+            gameObject.SetActive (!gameObject.activeInHierarchy);
         }
-    }
 
-    private void Start ()
-    {
-        SelectedItemId = -1;
-        _icons = new Dictionary <int, GameObject> ();
-        SelectedItemInfoObject.SetActive (false);
-        DestroySelectedObjectButtonObject.SetActive (false);
-
-        if (SourceUnit.MyStorage != null)
+        public void DestroySelectedItem ()
         {
-            foreach (var item in SourceUnit.MyStorage.Items)
+            if (SelectedItemId != -1 && SourceUnit.MyStorage [SelectedItemId] != null)
             {
-                AddItemUIItem (item);
+                SourceUnit.MyStorage.RemoveItem (SourceUnit.MyStorage [SelectedItemId]);
             }
         }
 
-        EventsHub.Instance.Subscribe (this, Storage.EventItemAdded);
-        EventsHub.Instance.Subscribe (this, Storage.EventItemRemoved);
-    }
-
-    private void OnDestroy ()
-    {
-        EventsHub.Instance.Subscribe (this, Storage.EventItemAdded);
-        EventsHub.Instance.Subscribe (this, Storage.EventItemRemoved);
-    }
-
-    private void Update ()
-    {
-        if (SelectedItemId != -1)
+        private void Start ()
         {
-            var item = SourceUnit.MyStorage [SelectedItemId];
+            SelectedItemId = -1;
+            _icons = new Dictionary <int, GameObject> ();
+            SelectedItemInfoObject.SetActive (false);
+            DestroySelectedObjectButtonObject.SetActive (false);
+
+            if (SourceUnit.MyStorage != null)
+            {
+                foreach (var item in SourceUnit.MyStorage.Items)
+                {
+                    AddItemUIItem (item);
+                }
+            }
+
+            EventsHub.Instance.Subscribe (this, Storage.EventItemAdded);
+            EventsHub.Instance.Subscribe (this, Storage.EventItemRemoved);
+        }
+
+        private void OnDestroy ()
+        {
+            EventsHub.Instance.Subscribe (this, Storage.EventItemAdded);
+            EventsHub.Instance.Subscribe (this, Storage.EventItemRemoved);
+        }
+
+        private void Update ()
+        {
+            if (SelectedItemId != -1)
+            {
+                var item = SourceUnit.MyStorage [SelectedItemId];
+                var itemType = SourceUnit.ItemTypesContainerRef.ItemTypes [item.ItemTypeId];
+                var infoBuilder = new StringBuilder ();
+
+                infoBuilder.Append (itemType.ShortInfo).AppendLine ().Append ("Level: ").Append (item.Level)
+                    .Append (".")
+                    .AppendLine ().Append ("Charge: ").Append (Math.Round (item.Charge * 10.0f) / 10.0f).Append ("/")
+                    .Append (
+                        Math.Round ((itemType.MaxCharge + itemType.MaxChargeAdditionPerLevel * item.Level) * 10.0f) /
+                        10.0f).Append (".").AppendLine ();
+
+                SelectedItemInfoObject.GetComponent <Text> ().text = infoBuilder.ToString ();
+            }
+        }
+
+        private void AddItemUIItem (Item item)
+        {
+            if (_icons.ContainsKey (item.Id))
+            {
+                return;
+            }
+
+            var itemUIItem = Instantiate (ItemUIItemPrefab);
+            _icons.Add (item.Id, itemUIItem);
+
+            itemUIItem.transform.SetParent (ItemsListContentObject.transform);
+            itemUIItem.transform.localScale = Vector3.one;
+
             var itemType = SourceUnit.ItemTypesContainerRef.ItemTypes [item.ItemTypeId];
-            var infoBuilder = new StringBuilder ();
+            var image = itemUIItem.GetComponent <Image> ();
+            image.sprite = itemType.Icon;
 
-            infoBuilder.Append (itemType.ShortInfo).AppendLine ().Append ("Level: ").Append (item.Level).Append (".")
-                .AppendLine ().Append ("Charge: ").Append (Math.Round (item.Charge * 10.0f) / 10.0f).Append ("/")
-                .Append (Math.Round ((itemType.MaxCharge + itemType.MaxChargeAdditionPerLevel * item.Level) * 10.0f) /
-                         10.0f).Append (".").AppendLine ();
-
-            SelectedItemInfoObject.GetComponent <Text> ().text = infoBuilder.ToString ();
-        }
-    }
-
-    private void AddItemUIItem (Item item)
-    {
-        if (_icons.ContainsKey (item.Id))
-        {
-            return;
+            var eventTrigger = itemUIItem.GetComponent <EventTrigger> ();
+            var entry = new EventTrigger.Entry {eventID = EventTriggerType.PointerClick};
+            entry.callback.AddListener (argument => { SendMessage ("ItemUIItemClicked", item.Id); });
+            eventTrigger.triggers.Add (entry);
         }
 
-        var itemUIItem = Instantiate (ItemUIItemPrefab);
-        _icons.Add (item.Id, itemUIItem);
-
-        itemUIItem.transform.SetParent (ItemsListContentObject.transform);
-        itemUIItem.transform.localScale = Vector3.one;
-
-        var itemType = SourceUnit.ItemTypesContainerRef.ItemTypes [item.ItemTypeId];
-        var image = itemUIItem.GetComponent <Image> ();
-        image.sprite = itemType.Icon;
-
-        var eventTrigger = itemUIItem.GetComponent <EventTrigger> ();
-        var entry = new EventTrigger.Entry {eventID = EventTriggerType.PointerClick};
-        entry.callback.AddListener (argument => { SendMessage ("ItemUIItemClicked", item.Id); });
-        eventTrigger.triggers.Add (entry);
-    }
-
-    private void RemoveItemUIItem (int itemId)
-    {
-        if (_icons.ContainsKey (itemId))
+        private void RemoveItemUIItem (int itemId)
         {
-            Destroy (_icons [itemId]);
-            _icons.Remove (itemId);
+            if (_icons.ContainsKey (itemId))
+            {
+                Destroy (_icons [itemId]);
+                _icons.Remove (itemId);
+            }
         }
-    }
 
-    private void ItemAdded (object parameter)
-    {
-        var argument = parameter as Storage.ItemAddedOrRemovedEventData;
-        if (argument.EventStorage == SourceUnit.MyStorage)
+        private void ItemAdded (object parameter)
         {
-            AddItemUIItem (argument.EventStorage [argument.ItemId]);
+            var argument = parameter as Storage.ItemAddedOrRemovedEventData;
+            if (argument.EventStorage == SourceUnit.MyStorage)
+            {
+                AddItemUIItem (argument.EventStorage [argument.ItemId]);
+            }
         }
-    }
 
-    private void ItemRemoved (object parameter)
-    {
-        var argument = parameter as Storage.ItemAddedOrRemovedEventData;
-        if (argument.EventStorage == SourceUnit.MyStorage)
+        private void ItemRemoved (object parameter)
         {
-            RemoveItemUIItem (argument.ItemId);
+            var argument = parameter as Storage.ItemAddedOrRemovedEventData;
+            if (argument.EventStorage == SourceUnit.MyStorage)
+            {
+                RemoveItemUIItem (argument.ItemId);
 
-            if (SelectedItemId == argument.ItemId)
+                if (SelectedItemId == argument.ItemId)
+                {
+                    SelectedItemId = -1;
+                    SelectedItemInfoObject.SetActive (false);
+                    DestroySelectedObjectButtonObject.SetActive (false);
+                }
+            }
+        }
+
+        private void ItemUIItemClicked (object parameter)
+        {
+            int itemId = (int) parameter;
+            if (SourceUnit.MyStorage [itemId] != null)
+            {
+                SelectedItemId = itemId;
+                SelectedItemInfoObject.SetActive (true);
+                DestroySelectedObjectButtonObject.SetActive (true);
+            }
+            else
             {
                 SelectedItemId = -1;
                 SelectedItemInfoObject.SetActive (false);
                 DestroySelectedObjectButtonObject.SetActive (false);
             }
-        }
-    }
-
-    private void ItemUIItemClicked (object parameter)
-    {
-        int itemId = (int) parameter;
-        if (SourceUnit.MyStorage [itemId] != null)
-        {
-            SelectedItemId = itemId;
-            SelectedItemInfoObject.SetActive (true);
-            DestroySelectedObjectButtonObject.SetActive (true);
-        }
-        else
-        {
-            SelectedItemId = -1;
-            SelectedItemInfoObject.SetActive (false);
-            DestroySelectedObjectButtonObject.SetActive (false);
         }
     }
 }
--- Assets/Source/Ingame/UI/SpellsBar.cs
+++ Assets/Source/Ingame/UI/SpellsBar.cs
@@ -10,148 +10,151 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class SpellsBar : MonoBehaviour
+namespace PriestOfPlague.Source.Ingame.UI
 {
-	public Unit SourceUnit;
-	public GameObject SpellUIItemPrefab;
-	public Text SpellMaxLevelIndicatorTextObject;
-	public InventoryBar InventoryBarRef;
-	
-	public const int MaxCastLevel = 10;
-	private Dictionary <int, GameObject> _icons;
-
-	private void Start () 
+	public class SpellsBar : MonoBehaviour
 	{
-		_icons = new Dictionary <int, GameObject> ();
-		if (SourceUnit.AvailableSpells != null)
+		public Unit.Unit SourceUnit;
+		public GameObject SpellUIItemPrefab;
+		public Text SpellMaxLevelIndicatorTextObject;
+		public InventoryBar InventoryBarRef;
+
+		public const int MaxCastLevel = 10;
+		private Dictionary <int, GameObject> _icons;
+
+		private void Start ()
 		{
-			foreach (var spellId in SourceUnit.AvailableSpells)
+			_icons = new Dictionary <int, GameObject> ();
+			if (SourceUnit.AvailableSpells != null)
 			{
-				AddSpellUIItem (spellId);
-			}
-		}
-
-		EventsHub.Instance.Subscribe (this, Unit.EventSpellLearned);
-		EventsHub.Instance.Subscribe (this, Unit.EventSpellForgotten);
-	}
-
-	private void OnDestroy ()
-	{
-		EventsHub.Instance.Unsubscribe (this, Unit.EventSpellLearned);
-		EventsHub.Instance.Unsubscribe (this, Unit.EventSpellForgotten);
-	}
-
-	private void Update ()
-	{
-		int maxCastLevelWithSelectedItem;
-		Item selectedItem = SourceUnit.MyStorage [InventoryBarRef.SelectedItemId];
-		
-		UpdateMaxLevelIndicatorText (out maxCastLevelWithSelectedItem, selectedItem);
-		ProcessInput (maxCastLevelWithSelectedItem, selectedItem);
-	}
-
-	private void UpdateMaxLevelIndicatorText (out int maxCastLevelWithSelectedItem, Item selectedItem)
-	{
-		if (SourceUnit.CurrentlyCasting == null)
-		{
-			SpellMaxLevelIndicatorTextObject.text = "~";
-		}
-
-		maxCastLevelWithSelectedItem = 0;
-		selectedItem = SourceUnit.MyStorage [InventoryBarRef.SelectedItemId];
-
-		if (selectedItem == null)
-		{
-			SpellMaxLevelIndicatorTextObject.text = "~";
-			return;
-		}
-
-		for (int index = 1; index <= Math.Min (MaxCastLevel, selectedItem.Level); index++)
-		{
-			if (SourceUnit.CanCast (index, selectedItem))
-			{
-				maxCastLevelWithSelectedItem = index;
-			}
-		}
-
-		if (maxCastLevelWithSelectedItem > 0)
-		{
-			SpellMaxLevelIndicatorTextObject.text = maxCastLevelWithSelectedItem.ToString ();
-		}
-		else
-		{
-			SpellMaxLevelIndicatorTextObject.text = "~";
-		}
-	}
-	
-	private void ProcessInput (int maxCastLevelWithSelectedItem, Item selectedItem)
-	{
-		if (maxCastLevelWithSelectedItem > 0)
-		{
-			for (int code = 1; code <= maxCastLevelWithSelectedItem; code++)
-			{
-				var keyCode = KeyCode.Alpha0 + (code % 10);
-				if (Input.GetKeyDown (keyCode))
+				foreach (var spellId in SourceUnit.AvailableSpells)
 				{
-					SourceUnit.CastSpell (code, selectedItem);
-					break;
+					AddSpellUIItem (spellId);
+				}
+			}
+
+			EventsHub.Instance.Subscribe (this, Unit.Unit.EventSpellLearned);
+			EventsHub.Instance.Subscribe (this, Unit.Unit.EventSpellForgotten);
+		}
+
+		private void OnDestroy ()
+		{
+			EventsHub.Instance.Unsubscribe (this, Unit.Unit.EventSpellLearned);
+			EventsHub.Instance.Unsubscribe (this, Unit.Unit.EventSpellForgotten);
+		}
+
+		private void Update ()
+		{
+			int maxCastLevelWithSelectedItem;
+			Item selectedItem = SourceUnit.MyStorage [InventoryBarRef.SelectedItemId];
+
+			UpdateMaxLevelIndicatorText (out maxCastLevelWithSelectedItem, selectedItem);
+			ProcessInput (maxCastLevelWithSelectedItem, selectedItem);
+		}
+
+		private void UpdateMaxLevelIndicatorText (out int maxCastLevelWithSelectedItem, Item selectedItem)
+		{
+			if (SourceUnit.CurrentlyCasting == null)
+			{
+				SpellMaxLevelIndicatorTextObject.text = "~";
+			}
+
+			maxCastLevelWithSelectedItem = 0;
+			selectedItem = SourceUnit.MyStorage [InventoryBarRef.SelectedItemId];
+
+			if (selectedItem == null)
+			{
+				SpellMaxLevelIndicatorTextObject.text = "~";
+				return;
+			}
+
+			for (int index = 1; index <= Math.Min (MaxCastLevel, selectedItem.Level); index++)
+			{
+				if (SourceUnit.CanCast (index, selectedItem))
+				{
+					maxCastLevelWithSelectedItem = index;
+				}
+			}
+
+			if (maxCastLevelWithSelectedItem > 0)
+			{
+				SpellMaxLevelIndicatorTextObject.text = maxCastLevelWithSelectedItem.ToString ();
+			}
+			else
+			{
+				SpellMaxLevelIndicatorTextObject.text = "~";
+			}
+		}
+
+		private void ProcessInput (int maxCastLevelWithSelectedItem, Item selectedItem)
+		{
+			if (maxCastLevelWithSelectedItem > 0)
+			{
+				for (int code = 1; code <= maxCastLevelWithSelectedItem; code++)
+				{
+					var keyCode = KeyCode.Alpha0 + (code % 10);
+					if (Input.GetKeyDown (keyCode))
+					{
+						SourceUnit.CastSpell (code, selectedItem);
+						break;
+					}
 				}
 			}
 		}
-	}
 
-	private void AddSpellUIItem (int spellId)
-	{
-		if (_icons.ContainsKey (spellId))
+		private void AddSpellUIItem (int spellId)
 		{
-			return;
+			if (_icons.ContainsKey (spellId))
+			{
+				return;
+			}
+
+			var spell = SourceUnit.SpellsContainerRef.Spells [spellId];
+			var spellUIItem = Instantiate (SpellUIItemPrefab);
+			_icons.Add (spellId, spellUIItem);
+
+			spellUIItem.transform.SetParent (transform);
+			spellUIItem.transform.localScale = Vector3.one;
+
+			var image = spellUIItem.GetComponent <Image> ();
+			image.sprite = spell.Icon;
+
+			var eventTrigger = spellUIItem.GetComponent <EventTrigger> ();
+			var entry = new EventTrigger.Entry {eventID = EventTriggerType.PointerClick};
+			entry.callback.AddListener (argument => { SendMessage ("SpellUIItemClicked", spellId); });
+			eventTrigger.triggers.Add (entry);
 		}
-		
-		var spell = SourceUnit.SpellsContainerRef.Spells [spellId];
-		var spellUIItem = Instantiate (SpellUIItemPrefab);
-		_icons.Add (spellId, spellUIItem);
-		
-		spellUIItem.transform.SetParent (transform);
-		spellUIItem.transform.localScale = Vector3.one;
 
-		var image = spellUIItem.GetComponent <Image> ();
-		image.sprite = spell.Icon;
-
-		var eventTrigger = spellUIItem.GetComponent <EventTrigger> ();
-		var entry = new EventTrigger.Entry {eventID = EventTriggerType.PointerClick};
-		entry.callback.AddListener (argument => { SendMessage ("SpellUIItemClicked", spellId); });
-		eventTrigger.triggers.Add (entry);
-	}
-
-	private void RemoveSpellUIItem (int spellId)
-	{
-		if (_icons.ContainsKey (spellId))
+		private void RemoveSpellUIItem (int spellId)
 		{
-			Destroy (_icons [spellId]);
-			_icons.Remove (spellId);
+			if (_icons.ContainsKey (spellId))
+			{
+				Destroy (_icons [spellId]);
+				_icons.Remove (spellId);
+			}
 		}
-	}
 
-	private void SpellLearned (object parameter)
-	{
-		var argument = parameter as Unit.SpellLearnedOrForgottenEventData;
-		if (argument.EventUnit == SourceUnit)
+		private void SpellLearned (object parameter)
 		{
-			AddSpellUIItem (argument.SpellId);
+			var argument = parameter as Unit.Unit.SpellLearnedOrForgottenEventData;
+			if (argument.EventUnit == SourceUnit)
+			{
+				AddSpellUIItem (argument.SpellId);
+			}
 		}
-	}
-	
-	private void SpellForgotten (object parameter)
-	{
-		var argument = parameter as Unit.SpellLearnedOrForgottenEventData;
-		if (argument.EventUnit == SourceUnit)
-		{
-			RemoveSpellUIItem (argument.SpellId);
-		}
-	}
 
-	private void SpellUIItemClicked (object parameter)
-	{
-		SourceUnit.StartCastingSpell (SourceUnit.SpellsContainerRef.Spells [(int) parameter]);
+		private void SpellForgotten (object parameter)
+		{
+			var argument = parameter as Unit.Unit.SpellLearnedOrForgottenEventData;
+			if (argument.EventUnit == SourceUnit)
+			{
+				RemoveSpellUIItem (argument.SpellId);
+			}
+		}
+
+		private void SpellUIItemClicked (object parameter)
+		{
+			SourceUnit.StartCastingSpell (SourceUnit.SpellsContainerRef.Spells [(int) parameter]);
+		}
 	}
 }
