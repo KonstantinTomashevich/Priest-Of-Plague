64f3b21c941e79c4ff349cc35b9974dd87c536bb
Konstantin Tomashevich <ekit.mail@gmail.com>
2018-06-01 17:24:15 +0300

    Add UnitSpawner script.

---
+++ Assets/Source/Ingame/UnitSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+namespace PriestOfPlague.Source.Ingame
+{
+	public class UnitSpawner : MonoBehaviour
+	{
+		public TextAsset XmlDocumentText;
+
+		IEnumerator Start ()
+		{
+			var document = new XmlDocument ();
+			document.LoadXml (XmlDocumentText.text);
+
+			var unit = gameObject.AddComponent <Unit.Unit> ();
+			yield return null;
+			unit.LoadFromXML (document.GetElementsByTagName ("unit") [0]);
+		}
+	}
+}
