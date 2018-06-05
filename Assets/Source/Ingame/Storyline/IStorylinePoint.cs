f998958de1efc8a207298d42dc86b64cac76ca3b
Konstantin Tomashevich <ekit.mail@gmail.com>
2018-06-05 19:28:38 +0300

    Add foundation for storyline system.

---
+++ Assets/Source/Ingame/Storyline/IStorylinePoint.cs
@@ -0,0 +1,10 @@
+﻿namespace PriestOfPlague.Source.Ingame.Storyline
+{
+    public interface IStorylinePoint
+    {
+        bool ShouldExit { get; }
+        void Enter ();
+        void Update ();
+        int Exit ();
+    }
+}
