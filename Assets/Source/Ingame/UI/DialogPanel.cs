a1c27adf73491afa4651029930a57867e09be900
Konstantin Tomashevich <ekit.mail@gmail.com>
2018-06-05 10:02:32 +0300

    Add DialogPanel.

---
+++ Assets/Source/Ingame/UI/DialogPanel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogPanel : MonoBehaviour
+{
+    public delegate void OnReply ();
+    public Text DialogText;
+    public Text FirstReplyText;
+    public Text SecondReplyText;
+
+    private OnReply _onFirst;
+    private OnReply _onSecond;
+
+    public void ReplyPressed (int replyIndex)
+    {
+        if (replyIndex == 0)
+        {
+            _onFirst ();
+        }
+        else
+        {
+            _onSecond ();
+        }
+        gameObject.SetActive (false);
+    }
+
+    public void Show (string text, string firstReply, string secondReply, OnReply onFirst, OnReply onSecond)
+    {
+        DialogText.text = text;
+        FirstReplyText.text = firstReply;
+        SecondReplyText.text = secondReply;
+
+        _onFirst = onFirst;
+        _onSecond = onSecond;
+        gameObject.SetActive (true);
+    }
+
+    private void Start ()
+    {
+        gameObject.SetActive (false);
+    }
+}
